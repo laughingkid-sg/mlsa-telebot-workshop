d5a5d572b56aa6bde0ea652d2fefd816498df07b
Zheng Teck <adamgoh1998@gmail.com>
2021-08-22 08:31:17 +0000

    Added Azure Text Analytics

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-// SS
-const { Telegraf } = require('telegraf')
-const azure = require('./azure')
+const { Telegraf } = require("telegraf")
+const azure = require("./azure")
 
-const bot = new Telegraf(process.env['BOT_TOKEN'])
+const bot = new Telegraf(process.env["BOT_TOKEN"])
 
-bot.start( async (ctx) => {
-    ctx.reply('Hey ' + ctx.update.message.from.first_name + "!") 
+bot.start(async (ctx) => {
+    ctx.reply("Hey " + ctx.update.message.from.first_name + "!")
 })
 
-bot.on('message', async (ctx) => {
-    // ctx.reply('You said ' + ctx.update.message.text)
+bot.on("message", async (ctx) => {
+    // ctx.reply("You said " + ctx.update.message.text)
+    azure.textanalytic(ctx.update.message.text)
     ctx.reply(await azure.qnamaker(ctx.update.message.text))
 })
 
 bot.launch()
 
-/* 
+/*
 
 Webhooks
 
 // Setting the webhook URL
-bot.telegram.setWebhook('URL')
+bot.telegram.setWebhook("URL")
 
 // Starting the Bot
-bot.startWebhook('/secret-path')
+bot.startWebhook("/secret-path")
 
 // Start the web server
-require('http')
-    .createServer(bot.webhookCallback('/secret-path'))
+require("http")
+    .createServer(bot.webhookCallback("/secret-path"))
     .listen(8080)
 
 */
--- azure.js
+++ azure.js
@@ -1,22 +1,49 @@
-const axios = require('axios');
+const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics")
+const axios = require("axios")
+
+const endpoint_url = ""
+
+const textAnalyticsClient = new TextAnalyticsClient(
+    endpoint_url,
+    new AzureKeyCredential(process.env["TA_TOKEN"]))
 
 exports.qnamaker = async (msg) => {
 
     const config = {
-        method: 'post',
-        url: 'https://msla-telebot-demo-cs.azurewebsites.net/qnamaker/knowledgebases/' + process.env['QnA_PATH'] + '/generateAnswer',
+        method: "post",
+        url: "https://msla-telebot-demo-cs.azurewebsites.net/qnamaker/knowledgebases/" + process.env["QnA_PATH"] + "/generateAnswer",
         headers: {
-            'Authorization': process.env['QnA_TOKEN'],
-            'Content-type': 'application/json'
+            "Authorization": process.env["QnA_TOKEN"],
+            "Content-type": "application/json"
         },
         data: {
-            'question' : msg
+            "question": msg
         }
     }
 
     const respond = await axios(config)
     // console.log(respond.data)
-    return respond.data.answers[0].answer == 'No good match found in KB.'
+    return respond.data.answers[0].answer == "No good match found in KB."
         ? "Opps I didn't understand that 😕"
         : respond.data.answers[0].answer
+}
+
+exports.textanalytic = async (msg) => {
+
+    const sentimentResult = await textAnalyticsClient.analyzeSentiment([msg])
+    //console.log(sentimentResult)
+    
+    sentimentResult.forEach(document => {
+        console.log(`ID: ${document.id}`)
+        console.log(`\tDocument Sentiment: ${document.sentiment}`)
+        console.log(`\tDocument Scores:`)
+        console.log(`\t\tPositive: ${document.confidenceScores.positive.toFixed(2)} \tNegative: ${document.confidenceScores.negative.toFixed(2)} \tNeutral: ${document.confidenceScores.neutral.toFixed(2)}`)
+        console.log(`\tSentences Sentiment(${document.sentences.length}):`)
+        document.sentences.forEach(sentence => {
+            console.log(`\t\tSentence sentiment: ${sentence.sentiment}`)
+            console.log(`\t\tSentences Scores:`)
+            console.log(`\t\tPositive: ${sentence.confidenceScores.positive.toFixed(2)} \tNegative: ${sentence.confidenceScores.negative.toFixed(2)} \tNeutral: ${sentence.confidenceScores.neutral.toFixed(2)}`)
+        })
+    })
+
 }
